b32040d0a97c524e899f5566fa76949f902dfa18
rehan.khan <rehan.khan@reboot-conseil.com>
2023-02-05 19:15:31 +0100

    feat (backend): create user route added

---
+++ backend/src/api/user_api.rs
@@ -0,0 +1,20 @@
+use crate::{models::user_model::User, repository::mongodb_repo::MongoRepo};
+use mongodb::results::InsertOneResult;
+use rocket::{http::Status, serde::json::Json, State};
+
+#[post("/user", data = "<new_user>")]
+pub fn create_user(
+    db: &State<MongoRepo>,
+    new_user: Json<User>,
+) -> Result<Json<InsertOneResult>, Status> {
+    let data = User {
+        id: None,
+        name: new_user.name.to_owned(),
+        level: new_user.level.to_owned(),
+    };
+    let user_detail = db.create_user(data);
+    match user_detail {
+        Ok(user) => Ok(Json(user)),
+        Err(_) => Err(Status::InternalServerError),
+    }
+}
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+//add the modules
+mod api;
+mod models;
+mod repository;
+
+#[macro_use]
+extern crate rocket;
+
+//add imports below
+use api::user_api::create_user;
+use repository::mongodb_repo::MongoRepo;
+
+use rocket::{get, http::Status, serde::json::Json};
+
+#[get("/")]
+fn hello() -> Result<Json<String>, Status> {
+    Ok(Json(String::from("Welcome to the API")))
+}
+
+#[launch]
+fn rocket() -> _ {
+    let db = MongoRepo::init();
+    rocket::build()
+        .manage(db)
+        .mount("/", routes![hello])
+        .mount("/", routes![create_user])
 }
